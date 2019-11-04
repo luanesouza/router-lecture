519b035db1969e81121c6a7b1ac7c0d906dd1bd9
luanesouza <annie.usa25@gmail.com>
2019-11-04 11:45:09 -0500

    adds css style to all pages

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,8 +4,11 @@ import AboutTheAuthor from './AboutTheAuthor';
 const Book = (props) => {
     return (
         <section class='bookInfo'>
-            <p id='title'>{props.book.title}</p>
-            <p id='author'>Written by {props.book.author}</p>
+            <p id='title'>{props.book.title}</p> 
+            <p id='author-name'>Written by {props.book.author}</p> 
+            <div class='book-cover-container'>
+            <img id='book-cover' src={props.book.bookCover} alt='book-cover'/>
+            </div>
             {/* <AboutTheAuthor author={props.book.author} about={props.book.aboutTheAuthor} image={props.book.img}/> */}
         </section>
 
